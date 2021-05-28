475016d4af9be40c7c6fdfbdb130c7ec3d33b4f0
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-28 16:05:34 +0500

    last

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <div className="header-container">
       <h1>Virtual lollipop</h1>
-      <h3>because we all know someone who deserves some sugar.</h3>
     </div>
   )
 }
