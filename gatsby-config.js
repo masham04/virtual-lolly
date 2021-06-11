43aa5203dcc543930d895b55325407f919a59007
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-06-11 07:19:40 +0500

    final

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,6 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "Lolly",
-        fieldName: "getAllLollies",
-        url: "https://mm-virtual-lolly.netlify.app/.netlify/functions/virtualLolly",
-      },
-    },
+   
   ],
 }
